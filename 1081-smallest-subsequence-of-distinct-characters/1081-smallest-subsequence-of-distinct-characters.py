285e870299a12ddbb34749e21e8e4c7419724fc2
Mayank Soni <Mayank.soni3@icloud.com>
2026-07-19 23:01:38 +0530

    Time: 4 ms (20.45%), Space: 12.4 MB (17.05%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+
+        last = {}
+        for i, ch in enumerate(s):
+            last[ch] = i
+
+        stack = []
+        seen = set()
+
+        for i, ch in enumerate(s):
+
+
+            if ch in seen:
+                continue
+
+
+            while stack and ch < stack[-1] and last[stack[-1]] > i:
+                seen.remove(stack.pop())
+
+            stack.append(ch)
+            seen.add(ch)
+
+        return "".join(stack)
