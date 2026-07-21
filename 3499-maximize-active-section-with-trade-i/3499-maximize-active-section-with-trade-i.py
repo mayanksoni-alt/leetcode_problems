215058e9dc451eb56af1a7d5d2800d265b5f5417
Mayank Soni <Mayank.soni3@icloud.com>
2026-07-21 23:36:51 +0530

    Time: 1340 ms (40%), Space: 13.2 MB (40%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxActiveSectionsAfterTrade(self, s):
+        ans = s.count("1")
+
+        pre = float("-inf")
+        mx = 0
+
+        i = 0
+        n = len(s)
+
+        while i < n:
+            j = i
+            while j < n and s[j] == s[i]:
+                j += 1
+
+            length = j - i
+
+            if s[i] == "0":
+                mx = max(mx, pre + length)
+                pre = length
+
+            i = j
+
+        return ans + mx
